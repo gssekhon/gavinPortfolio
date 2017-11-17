a5e3f1e48fc9ae4966e5f48eba99fa2ebd4cb601
Gavin Sekhon <gssekhon@sfu.ca>
2017-11-16 21:19:09 -0800

    whole lotta changes i didnt push

--- js/script.js
+++ js/script.js
@@ -2,10 +2,3 @@
 code learned from
 https://stackoverflow.com/questions/8114657/how-to-style-the-parent-element-when-hovering-a-child-element
 ***/
-
-$('#linkedin').on('mouseover', function(){
-  console.log("mouseover");
-  $(this).closest('.bg1z').addClass('#linkedin')
-}).on('mouseout', function(){
-  $(this).closest('.bg1z').removeClass('#linkedin');
-});
